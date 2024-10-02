e72eb7333d4e157fbf5974028a7b11f447653b3b
Traxar <trxr2a@gmail.com>
2024-10-02 08:23:51 +0200

    minor formating fix

--- src/updater.py
+++ src/updater.py
@@ -3,7 +3,7 @@ import sys
 from info import Task
 
 def install(*packages):
-    list = [ name for name in packages]
+    list = [name for name in packages]
     subprocess.check_call([sys.executable, '-m', 'pip', 'install'] + list) #add -q for quiet mode
 
 def run():
